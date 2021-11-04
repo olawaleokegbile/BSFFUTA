847645bc4af7a1a0c4fb47c26c323ca8801f7917
Olajuwon Rasheed <rasholajuwon@gmail.com>
2021-11-04 14:25:14 +0100

    New commit

--- bsffuta/settings.py
+++ bsffuta/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*ry8xs84138cxm0dd(h*7#xyee(%51f+c&enz(lm251gao#ei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['127.0.0.1', 'bsfapp.herokuapp.com', 'bsffuta.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'bsfapp.herokuapp.com', 'bsffuta.herokuapp.com']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -158,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
